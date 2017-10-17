48e9407c7786e54a9f918871ef45da223fdec8c5
Denis Nikiforov <popalay@ex.ua>
2017-10-17 15:04:47 +0300

    Add ViewModelFactory for instantiation of view models

--- presentation/src/main/kotlin/com/popalay/appname/injection/UiModule.kt
+++ presentation/src/main/kotlin/com/popalay/appname/injection/UiModule.kt
@@ -1,9 +1,15 @@
 package com.popalay.appname.injection
 
+import android.arch.lifecycle.ViewModelProvider
+import com.popalay.appname.base.ViewModelFactory
+import dagger.Binds
 import dagger.Module
 
 @Module
 abstract class UiModule {
+
+    @Binds
+    abstract fun provideViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 
     //TODO provide activity modules
 
